78ab4e8e08848b481baf66c74a4e5e674bdefe75
Mirzakhid Osmankulov <mirzakhidosmankulov@gmail.com>
2021-12-11 14:10:11 +0600

    Completing the tasks

--- lists/lists.py
+++ lists/lists.py
@@ -10,10 +10,7 @@ def add_elem_to_list(list, elem):
 def delete_elem_from_list(list, index = -1):
     # delete element from list, such that its index is index
     # if index is invalid, return empty list
-    if(index):
         list.pop(index)
-    else:
-        return list
 
 
 def count_elements_in_list(list, x):
