ade8b531694f17ffcd60272860e7643b43e2e847
Mirzakhid Osmankulov <mirzakhidosmankulov@gmail.com>
2021-12-11 13:56:36 +0600

    Completing the tasks

--- functions-packages/hypotenuse.py
+++ functions-packages/hypotenuse.py
@@ -1,4 +1,4 @@
 import math   
 def getHypotenuse(catet1, catet2):  
-    c = catet1 * catet1 + catet2 * catet2   
-    return round(math.sqrt(c))  
+    c = math.sqrt(catet1 * catet1 + catet2 * catet2)   
+    return 
